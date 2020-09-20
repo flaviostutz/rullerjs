d67c3f9db8de65172b32fbecbd80ca120e25271d
flaviostutz <flaviostutz@gmail.com>
2020-09-20 15:18:49 -0300

    working version before removing startPolling function (obsolete because of startMonitoring())

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ See a very simple demo at https://youtu.be/ScWwz1zUasI
 
 ## Usage
 
-* Run a sample ruller container for some rules
+* Run a sample ruller container with some rules
 
 ```sh
 git clone https://github.com/flaviostutz/ruller-sample-feature-flag
@@ -14,12 +14,13 @@ cd ruller-sample-feature-flag
 docker-compose up --build -d
 ```
 
-* Register RullerJS for invoking feature flag service and keepup with changes
+* Register RullerJS for invoking feature flag service and keep it up with changes by polling the service each 1s (bad for scalling!)
 
 ```js
 let input = {customerid: "123"}
 r = new RullerJS("http://127.0.0.1:8080/rules/domains", 
-    input, 
+    input,
+    0,
     (output) => {
         console.log("OUTPUT CHANGED: " + JSON.stringify(output))
         document.getElementById("status").innerText = JSON.stringify(output)
@@ -28,4 +29,51 @@ r = new RullerJS("http://127.0.0.1:8080/rules/domains",
     });
 r.startPolling(1000)
 ```
+
+* Register RullerJS for invoking feature flag service and keep it up with changes by monitoring a webservice (great for scale but requires an environment that supports websockets). When the ws closes, it will try to get newer flags.
+
+```js
+let input = {customerid: "123"}
+r = new RullerJS("http://127.0.0.1:8080/rules/domains",
+    input,
+    0,
+    (output) => {
+        console.log("OUTPUT CHANGED: " + JSON.stringify(output))
+        document.getElementById("status").innerText = JSON.stringify(output)
+    }, (err) => {
+        console.log("ERROR: " + err)
+    });
+r.startMonitoring("ws://127.0.0.1:8080/ws")
+```
+
+## API
+
+### new RullerJS(rullerURL, inputData, cacheSeconds, onChange, onErr)
+
+* Instantiates a new RullerJS service. RullerJS will issue a POST request to the ruller endpoint with "inputData" and if there is an outputData available, will call onChange(data). After instantiation it won't start polling or monitoring automatically. You have to call startPolling() or startMonitoring() for this to happen.
+
+* *rullerURL* - The full URL for the ruller service. ex.: "http://myflags.site.com/rules/domains"
+
+* *inputData* - object with plain data used as input for feature flag evaluation. Ex: `{customerid:"123456abc"}`
+
+* *cacheSeconds* - if > 0, will reuse the last successful outputData among page reloads while the ruller service is down. Useful to avoid the user to see the "default" page configurations while the flag service is down for a short time. The cached outputData is stored in localStorage.
+
+* *onChange* - callback function called when there is a useful outputData from flag service. It will be called when the contents of the flag service output is changed or when it is the first invocation, the service is down, but the cache (in localStorage) is still valid.
+
+* *onError* - callback function invoked when an error occurs during ruller service requests. Having an error here doesn't mean RullerJS will stop trying to reconnect if it is monitoring or polling the endpoint. If you want to stop trying, call RullerJS.stopMonitoring() or RullerJS.stopPolling().
+
+
+### RullerJS.startMonitoring(rullerWSURL, randomBackoffMaxMillis, maxRetryIntervalMillis)
+
+* Useful for detecting ruller service changes without the need to poll the service periodically. This will start monitoring a Websocket endpoint and if it is closed, will retry connecting to the ruller service to get newer data.
+
+* *rullerWSURL* - Websocket URL used for monitoring "closes". Ex.: "ws://myflags.site.com/ws". Use the same ruller websocket service as the POST service so that when your service is restarted, the new configurations will be fetched.
+
+* *randomBackoffMaxMillis* - When trying to reconnect to the ruller service, wait for a random time at the first retry so that the not all clients will execute a DDoS on servers. This value defines the max time in randomization.
+
+* *maxRetryIntervalMillis* - At each failed retry, the time to wait before trying again will double. This is the max time to wait before retrying, limiting the "doubling".
+
+### RullerJS.stopMonitoring()
+
+* Close websocket and cancel all monitoring activities of the ruller service. Changes in ruller service won't affect the client anymore.
 
--- rullerjs.js
+++ rullerjs.js
@@ -12,36 +12,101 @@
  */
 
 class RullerJS {
-    constructor(rullerURL, input, onChange, onErr) {
+    constructor(rullerURL, input, cacheMillis, onChange, onErr) {
         if (!rullerURL) {
             throw "rullerURL is required"
         }
         if (!onChange) {
             throw "onOutput is required"
         }
-        this.rullerURL = rullerURL;
-        this.onChange = onChange;
-        this.onErr = onErr;
-        this.input = input;
+        this.rullerURL = rullerURL
+        this.onChange = onChange
+        this.onErr = onErr
+        this.input = input
 
-        this.timerH = null;
-        this.previousData = null;
+        this.previousData = null
+        this.cacheMillis = cacheMillis
 
-        this.poll();
+        this.rullerWSURL = null
+        this.pollIntervalMillis = null
+        this.randomBackoffMaxMillis = null
+        this.maxRetryIntervalMillis = null
+        this.retryCount = 0
+        this.retryWaitTime = null
+        this.currentTimer = null
+
+        this.currentTimer = null
+
+        this.poll(false);
     }
 
-    startPolling(intervalMillis) {
-        this.timerH = setInterval(() => this.poll(), intervalMillis)
+    startPolling(pollIntervalMillis, randomBackoffMaxMillis, maxRetryIntervalMillis) {
+        this.stopMonitoring()
+        if(!pollIntervalMillis || pollIntervalMillis < 1000) {
+            throw "pollIntervalMillis is required and >= 1000ms"
+        }
+        if(!randomBackoffMaxMillis) {
+            randomBackoffMaxMillis = pollIntervalMillis * 3
+        }
+        if(!maxRetryIntervalMillis) {
+            maxRetryIntervalMillis = pollIntervalMillis * 6
+        }
+        if(randomBackoffMaxMillis < pollIntervalMillis) {
+            throw "randomBackoffMaxMillis cannot be less than pollIntervalMillis"
+        }
+        if(maxRetryIntervalMillis < pollIntervalMillis) {
+            throw "randomBackoffMaxMillis cannot be less than pollIntervalMillis"
+        }
+        this.pollIntervalMillis = pollIntervalMillis
+        this.randomBackoffMaxMillis = randomBackoffMaxMillis
+        this.maxRetryIntervalMillis = maxRetryIntervalMillis
+        this.schedule(null, true)
     }
 
     stopPolling() {
-        clearInterval(this.timerH)
-        this.timerH = null
+        if(this.currentTimer!=null) {
+            clearTimeout(this.currentTimer)
+            this.currentTimer = null
+        }
+        this.pollIntervalMillis = null
+        this.randomBackoffMaxMillis = null
+        this.maxRetryIntervalMillis = null
     }
 
-    poll() {
-        let _self = this
-        console.log("RullerJS: Polling ruller...")
+    startMonitoring(rullerWSURL, randomBackoffMaxMillis, maxRetryIntervalMillis) {
+        this.stopPolling()
+        if(!randomBackoffMaxMillis) {
+            randomBackoffMaxMillis = 12000
+        }
+        if(!maxRetryIntervalMillis) {
+            maxRetryIntervalMillis = 30000
+        }
+        this.rullerWSURL = rullerWSURL
+        this.randomBackoffMaxMillis = randomBackoffMaxMillis
+        this.maxRetryIntervalMillis = maxRetryIntervalMillis
+        this.schedule(null, true)
+    }
+
+    stopMonitoring() {
+        if(this.currentTimer!=null) {
+            clearTimeout(this.currentTimer)
+            this.currentTimer = null
+        }
+        this.rullerWSURL = null
+        this.randomBackoffMaxMillis = null
+        this.maxRetryIntervalMillis = null
+        if(this.ws!=null) {
+            this.ws.close()
+            this.ws = null
+        }
+    }
+
+    poll(applySchedule) {
+        if(applySchedule) {
+            this.currentTimer = null
+        }
+
+        // console.log("RullerJS: Polling ruller...")
         fetch(this.rullerURL, {
             method: 'POST',
             headers: {
@@ -52,47 +117,157 @@ class RullerJS {
 
         }).then((response) => {
             if(response.status!=200) {
-                console.log("RullerJS: Error fetching " + _self.rullerURL + ". status=" + response.status)
-                if(_self.onErr) {
-                    _self.onErr("Error fetching " + _self.rullerURL + ". status=" + response.status)
-                }
+                this.schedule("Error fetching " + this.rullerURL + ". status=" + response.status, applySchedule)
                 return
             }
-            response.json().then(function(data) {
-                console.log("RulleJS: got response: " + data)
-                if(_self.previousData == null || (JSON.stringify(_self.previousData) != JSON.stringify(data))) {
-                    console.log("Notifying client");
-                    _self.onChange(data)
-                    _self.previousData = data
-                } else {
-                    console.log("Ruller fetched but output is the same. Ignoring.")
+            
+            response.json()
+            .then((data) => {
+                // console.log("RullerJS: got response: " + data)
+                let ndata = JSON.stringify(data)
+                if(this.previousData == null || (JSON.stringify(this.previousData) != ndata)) {
+                    console.log("RullerJS flags: " + ndata);
+                    this.onChange(data)
+                    this.previousData = data
+
+                    if(this.cacheMillis>0) {
+                        localStorage.setItem("rullerjs:lastData", JSON.stringify({time:new Date().getTime(), data: data}))
+                    }
                 }
+                this.schedule(null, applySchedule)
+            }).catch((err) => {
+                this.schedule(err, applySchedule)
             });
 
-        }, (err) => {
-            const e = "Error fetching " + _self.rullerURL + ". err=" + err
-            if(_self.onErr) {
-                _self.onErr(e)
-            } else {
-                console.log(e)
-            }
-
         }).catch((err) => {
-            const e = "Error fetching " + _self.rullerURL + ". err=" + err
-            if(_self.onErr) {
-                _self.onErr(e)
-            } else {
-                console.log(e)
-            }
+            this.schedule("Error fetching " + this.rullerURL + ". err=" + err, applySchedule)
         })
     }
 
     setInput(input) {
         this.input = input
-        if(this.timerH==null) {
-            this.poll()
+        this.poll(false)
+    }
+
+    schedule(lastErr, applySchedule) {
+
+        if(lastErr) {
+            console.log("RullerJS: " + lastErr)
+            if(this.onErr) {
+                this.onErr(lastErr)
+            }
+        }
+
+        //LOCALSTORAGE CACHE
+        if(!applySchedule) {
+            if(lastErr && this.cacheMillis>0) {
+
+                let cachedData = localStorage.getItem("rullerjs:lastData")
+                if(cachedData==null) {
+                    return
+                }
+
+                let lcache = null
+                try {
+                    lcache = JSON.parse(cachedData)
+                } catch (err) {
+                    console.log("RullerJS: error parsing localstorage cache data. err=" + err)
+                    return
+                }
+
+                if(lcache.data==null || lcache.time==null) {
+                    return
+                }
+
+                let elapsedCache = (new Date().getTime()-lcache.time)
+                if(!(elapsedCache <= this.cacheMillis)) {
+                    console.log("RullerJS: cache expired")
+                    localStorage.removeItem("rullerjs:lastData")
+                    return
+                }
+
+                if(this.previousData == null || (JSON.stringify(this.previousData) != JSON.stringify(lcache.data))) {
+                    console.log("RullerJS flags (cached): " + lcache.data);
+                    this.onChange(lcache.data)
+                    this.previousData = lcache.data
+                }
+            }
+            return
+        }
+
+        if(this.pollIntervalMillis == null && this.rullerWSURL == null) {
+            return
+        }
+
+        if(this.currentTimer) {
+            console.log("RullerJS: skipping overlapped schedule")
+            return
+        }
+
+        if(lastErr) {
+            this.retryCount++
+        } else {
+            if(this.retryCount>0) {
+                console.log("RullerJS: connection restored")
+            }
+            this.retryCount = 0
+            this.retryWaitTime = null
+        }
+
+        //RETRY FROM FAILURE
+        //define first retry random backoff to avoid too much pressure on server when
+        //it restarts so that not all clients would connect back at the same time
+        if(this.retryCount > 0) {
+            let minInterval = this.pollIntervalMillis
+            if(minInterval == null) {
+                minInterval = Math.min(3000, this.randomBackoffMaxMillis)
+            }
+            if(this.retryCount == 1) {
+                // console.log("Random backoff retry delay")
+                this.retryWaitTime = getRandomIntInclusive(minInterval, this.randomBackoffMaxMillis)
+            } else if(this.retryCount == 2) {
+                // console.log("retryCount==2")
+                this.retryWaitTime = minInterval
+            } else {
+                // console.log("retryCount>2")
+                this.retryWaitTime = Math.min(this.retryWaitTime * 2, this.maxRetryIntervalMillis)
+            }
+            console.log("RullerJS: Retrying in " + this.retryWaitTime + "ms")
+            if(this.retryWaitTime>=1000) {
+                this.currentTimer = setTimeout(() => this.poll(true), this.retryWaitTime)
+            }
+            return
+        }
+
+        //POLLING FOR CHANGES
+        if(this.pollIntervalMillis != null) {
+            // console.log("Polling in " + this.pollIntervalMillis + "ms")
+            this.pending = true
+            if(this.pollIntervalMillis>=1000) {
+                this.currentTimer = setTimeout(() => this.poll(true), this.pollIntervalMillis)
+            }
+        }
+
+        //MONITOR WS CLOSE FOR CHANGES
+        if(this.rullerWSURL != null) {
+            var ws = new WebSocket(this.rullerWSURL);
+            ws.onopen = () => {
+                // console.log("RullerJS: connected to " + this.rullerWSURL)
+            };
+            ws.onclose = (err) => {
+                this.schedule("websocket closed. err=" + err, true)
+            };
+            ws.onerror = (err) => {
+              this.schedule("websocket err=" + err, true)
+              ws.close()
+            };
+            this.ws = ws
         }
     }
-    
 }
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
